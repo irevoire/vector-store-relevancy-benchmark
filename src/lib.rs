eabb993059c8c83570012da58d1809ecb18e8f0e
Tamo <tamo@meilisearch.com>
2024-09-03 18:13:16 +0200

    move the dataset to their own module

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 mod arroy_bench;
+mod dataset;
 mod qdrant;
 
-use std::fs::File;
-use std::mem;
-use std::{fmt, marker::PhantomData};
+pub use dataset::*;
+
+use std::fmt;
 
 use arroy::distances::*;
 use arroy::{
@@ -11,8 +12,6 @@ use arroy::{
     ItemId,
 };
 use arroy_bench::measure_arroy_distance;
-use bytemuck::{AnyBitPattern, PodCastError};
-use memmap2::Mmap;
 use qdrant_client::qdrant::{quantization_config, ScalarQuantizationBuilder};
 
 use crate::qdrant::measure_qdrant_distance;
@@ -112,71 +111,6 @@ fn bench_qdrant_distance<D: Distance>() -> &'static (dyn Fn(usize, &[(u32, &[f32
     &measure_qdrant_distance::<D> as &dyn Fn(usize, &[(u32, &[f32])])
 }
 
-pub struct MatLEView<T> {
-    name: &'static str,
-    mmap: Mmap,
-    dimensions: usize,
-    _marker: PhantomData<T>,
-}
-
-impl<T: AnyBitPattern> MatLEView<T> {
-    pub fn new(name: &'static str, path: &str, dimensions: usize) -> MatLEView<T> {
-        let file = File::open(path).unwrap();
-        let mmap = unsafe { Mmap::map(&file).unwrap() };
-
-        assert!((mmap.len() / mem::size_of::<T>()) % dimensions == 0);
-        MatLEView {
-            name,
-            mmap,
-            dimensions,
-            _marker: PhantomData,
-        }
-    }
-
-    pub fn header(&self) {
-        println!(
-            "{} - {} vectors of {} dimensions",
-            self.name,
-            self.len(),
-            self.dimensions
-        );
-    }
-
-    pub fn dimensions(&self) -> usize {
-        self.dimensions
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.len() == 0
-    }
-
-    pub fn len(&self) -> usize {
-        (self.mmap.len() / mem::size_of::<T>()) / self.dimensions
-    }
-
-    pub fn get(&self, index: usize) -> Option<Result<&[T], PodCastError>> {
-        let tsize = mem::size_of::<T>();
-        if (index * self.dimensions + self.dimensions) * tsize < self.mmap.len() {
-            let start = index * self.dimensions;
-            let bytes = &self.mmap[start * tsize..(start + self.dimensions) * tsize];
-            match bytemuck::try_cast_slice::<u8, T>(bytes) {
-                Ok(slice) => Some(Ok(slice)),
-                Err(e) => Some(Err(e)),
-            }
-        } else {
-            None
-        }
-    }
-
-    pub fn iter(&self) -> impl Iterator<Item = &[T]> {
-        (0..self.len() - 1).map(|i| self.get(i).unwrap().unwrap())
-    }
-
-    pub fn get_all(&self) -> Vec<&[T]> {
-        self.iter().collect()
-    }
-}
-
 fn partial_sort_by<'a, D: arroy::Distance>(
     mut vectors: impl Iterator<Item = (ItemId, &'a [f32])>,
     sort_by: &[f32],
@@ -247,33 +181,3 @@ impl fmt::Debug for Recall {
         write!(f, "{:.2}\x1b[0m", self.0)
     }
 }
-
-pub const HN_TOP_POSTS_PATH: &str = "assets/hn-top-posts.mat";
-pub const HN_TOP_POSTS_DIMENSIONS: usize = 1024;
-pub fn hn_top_posts() -> MatLEView<f32> {
-    MatLEView::new(
-        "Hackernews top posts",
-        HN_TOP_POSTS_PATH,
-        HN_TOP_POSTS_DIMENSIONS,
-    )
-}
-
-pub const HN_POSTS_PATH: &str = "assets/hn-posts.mat";
-pub const HN_POSTS_DIMENSIONS: usize = 512;
-pub fn hn_posts() -> MatLEView<f32> {
-    MatLEView::new("Hackernews posts", HN_POSTS_PATH, HN_POSTS_DIMENSIONS)
-}
-
-pub const DB_PEDIA_OPENAI_TEXT_EMBEDDING_3_LARGE_PATH: &str =
-    "assets/db-pedia-OpenAI-text-embedding-3-large.mat";
-pub const DB_PEDIA_OPENAI_TEXT_EMBEDDING_3_LARGE_DIMENSIONS: usize = 3072;
-// pub fn hn_posts() -> MatLEView<f32> {
-//     MatLEView::new("Hackernews posts", "assets/hn-posts.mat", 512)
-// }
-
-pub const DB_PEDIA_OPENAI_TEXT_EMBEDDING_ADA_002_PATH: &str =
-    "assets/db-pedia-OpenAI-text-embedding-3-large.mat";
-pub const DB_PEDIA_OPENAI_TEXT_EMBEDDING_ADA_002_DIMENSIONS: usize = 1536;
-// pub fn hn_posts() -> MatLEView<f32> {
-//     MatLEView::new("Hackernews posts", "assets/hn-posts.mat", 512)
-// }
